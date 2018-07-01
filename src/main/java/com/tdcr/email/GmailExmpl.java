c5c6f77fa9f3bc1cb566f5607377153cf9aaa66f
theDarkCoderRises <thedarkcoderrises@gmail.com>
2018-07-01 16:12:21 +0530

    Initial commit

---
+++ src/main/java/com/tdcr/email/GmailExmpl.java
@@ -0,0 +1,54 @@
+package com.tdcr.email;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+/**
+ * Hello world!
+ *
+ */
+//need to enable lessSecure in gmail
+//https://myaccount.google.com/lesssecureapps?pli=1
+public class GmailExmpl
+{
+    public static void main( String[] args )
+    {
+        final String username = "abhikrish88@gmail.com";
+        final String password = "";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("abhikrish88@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse("abhikrish88@gmail.com"));
+            message.setSubject("Testing Subject");
+            message.setText("Dear Mail Crawler,"
+                    + "\n\n No spam to my email, please!");
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
---
+++ src/test/java/com/tdcr/email/GmailExmplTest.java
@@ -0,0 +1,20 @@
+package com.tdcr.email;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple GmailExmpl.
+ */
+public class GmailExmplTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
